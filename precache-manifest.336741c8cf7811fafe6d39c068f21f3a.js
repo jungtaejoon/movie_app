c4b07b6f965d8cc759bb318204c9dc800c858926
jungtaejoon <jungtaejoon@naver.com>
2018-10-28 01:05:32 +0900

    Updates

---
+++ precache-manifest.336741c8cf7811fafe6d39c068f21f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "9edea8c13a6e8782c5e9",
+    "url": "/movie_app/static/js/main.9edea8c1.chunk.js"
+  },
+  {
+    "revision": "46602178259828021dee",
+    "url": "/movie_app/static/js/1.46602178.chunk.js"
+  },
+  {
+    "revision": "9edea8c13a6e8782c5e9",
+    "url": "/movie_app/static/css/main.751d0825.chunk.css"
+  },
+  {
+    "revision": "2b1ff764d87d036f1e739f2813d7152f",
+    "url": "/movie_app/index.html"
+  }
+];
